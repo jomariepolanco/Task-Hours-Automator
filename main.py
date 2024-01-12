f37c4bf5fa67af4736396a9ae93fe1a19ce56fab
jomariepolanco <jomariepolanco@gmail.com>
2024-01-11 21:45:01 -0800

    psuedocode

---
+++ main.py
@@ -0,0 +1,7 @@
+# Connect to Google Calendar
+# Connect to VS Code
+# Determine start time in vscode
+# Open directory/file in vscode
+# Determine end time in vscode
+# Git commit -m “” message
+# Log difference on google calendar with project name as title
